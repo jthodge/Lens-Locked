1c967572b11d590504219ff8255a895e23ad5bc2
Taylor Hodge <j.taylor.hodge@gmail.com>
2019-08-04 13:00:08 -0400

    Add routing for custom 404 page

--- main.go
+++ main.go
@@ -25,10 +25,20 @@ func faq(w http.ResponseWriter, r *http.Request) {
     "asked questions about LensLocked.")
 }
 
+func pageNotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>We couldn't find the page you're " +
+	" looking for.</h1>" + "<p>Please " +
+	"<a href=\"mailto:support@lenslocked.com\">email us</a> if " + 
+	"you continue to experience this issue.</p>")
+}
+
 func main() {
+	var notFound http.Handler = http.HandlerFunc(pageNotFound)
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = notFound
 	http.ListenAndServe(":3000", r)
 }
