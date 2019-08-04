bc7d38c9ac964d6c00a38d051b3b168dde18c41a
Taylor Hodge <j.taylor.hodge@gmail.com>
2019-08-04 12:53:04 -0400

    Add route for FAQ page

--- main.go
+++ main.go
@@ -19,9 +19,16 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		"support@lenslocked.com</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "This page answers the most frequently " +
+    "asked questions about LensLocked.")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
