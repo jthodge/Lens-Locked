e3748599bfc841bce29ca1bc7e35a36f1a615567
Taylor Hodge <j.taylor.hodge@gmail.com>
2019-08-04 12:50:06 -0400

    Refactor routing with gorilla/mux

--- main.go
+++ main.go
@@ -3,26 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome!</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, send a message to " +
-			"<a href=\"mailto:support@lenslocked.com\">" +
-		  "support@lenslocked.com</a>.")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>We couldn't find the page you're " +
-		" looking for.</h1>" + "<p>Please " +
-		"<a href=\"mailto:support@lenslocked.com\">email us</a> if " + 
-		"you continue to experience this issue.</p>")
-	}
+	fmt.Fprint(w, "<h1>Welcome!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, send a message to " +
+		"<a href=\"mailto:support@lenslocked.com\">" +
+		"support@lenslocked.com</a>.")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
