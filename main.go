8aa37375d9c659bea0fd3c0d9c9e6d9b0ae23f98
Taylor Hodge <j.taylor.hodge@gmail.com>
2019-08-04 12:20:41 -0400

    Add routing for 404 responses

--- main.go
+++ main.go
@@ -11,8 +11,14 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome!</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, send a message to " +
-			"<a href=\"mailto:support@lenslocked.com\">"+
+			"<a href=\"mailto:support@lenslocked.com\">" +
 		  "support@lenslocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We couldn't find the page you're " +
+		" looking for.</h1>" + "<p>Please " +
+		"<a href=\"mailto:support@lenslocked.com\">email us</a> if " + 
+		"you continue to experience this issue.</p>")
 	}
 }
 
