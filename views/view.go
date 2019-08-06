deddf262f47c9816db6ac87469d8eaf9c3565220
Taylor Hodge <j.taylor.hodge@gmail.com>
2019-08-05 23:35:30 -0400

    wip

--- views/view.go
+++ views/view.go
@@ -3,7 +3,10 @@ package views
 import "html/template"
 
 func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+	files = append(files,
+		"views/layouts/footer.gohtml",
+		"views/layouts/navbar.gohtml",
+		"views/layouts/bootstrap.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
